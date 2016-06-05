1f348e742f617271320a5f84b9ac25fcbd14e60d
wdewit <wendy.dewit@telenet.be>
2016-06-05 11:48:59 +0200

    Rewrote this to be more clearer in future

--- cachematrix.R
+++ cachematrix.R
@@ -1,21 +1,21 @@
 ## makeCacheMatrix creates a special "matrix" object that can cache its inverse
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(xinp = matrix()) {
   #initialize the inverse of the matrix 
-   minv <- NULL
+   minv_cache <- NULL
   #store a newly defined matrix (in the cachesolve function) in cache 
   set <- function(y) {
-    x <<- y
-    minv <<- NULL
+    xinp <<- y   # <<- assigns value of y to cached xinp object
+    minv_cache <<- NULL
   }
   #read the value of a previously defined matrix from the cache
-  get <- function() x
+  get <- function() xinp
   #store the value of a newly inverted matrix into cache
-  setinverse <- function(solve) minv <<- solve
+  setinverse <- function(y) minv_cache <<- y
   #read the value of a previously inverted matrix from the cache
-  getinverse <- function() minv
+  getinverse <- function() minv_cache
   #pass on the value of the function makeCacheMatrix
-  list(set = set, get = get,
+  list(set=set,get = get,
        setinverse = setinverse,
        getinverse = getinverse)
 }
@@ -27,17 +27,21 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-     minv <- x$getinverse
+     minv <- x$getinverse()
      #if the inverse exists already in cache, go and get it from there
      if (!is.null(minv)) {
        message("reading inverted matrix from cache")
        return(minv)
      }
+     else {
      #if there is no inverted matrix in cache, read matrix from cache
      data <- x$get()
      #calculate inverted matrix and store it in cache
      minv <- solve(data, ...)
      x$setinverse(minv)
      #show the inverted matrix
-     minv     
+     minv  
+     }
 }
+
+##first create cacheMatrix -> then run cachesolve. eg. b <- makeCacheMatrix(diag(5,3)) cacheSolve(b)
